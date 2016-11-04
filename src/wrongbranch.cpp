2b8f45eda551dd6bfc68f1e469f199a96be0a8f2
wrongbranch <wrongbranch@yahoo.co.jp>
2016-11-04 11:30:24 +0900

    test_sscanf()

--- src/wrongbranch.cpp
+++ src/wrongbranch.cpp
@@ -10,7 +10,26 @@
 #include <cstdlib>
 #include <string>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 using namespace std;
+
+#define BUFSIZE 8
+static char *src = "abcd\nefg\nhijklmn\nop";
+
+int test_sscanf()
+{
+  char fmt[BUFSIZE], buf[6];
+  sprintf(fmt, "%%%d[^\n]", sizeof(buf) - 1);
+  int readnum = sscanf(src, fmt, buf);
+  fprintf(stderr, "len(fmt): %d\n", strlen(fmt));
+  fprintf(stderr, "readnum: %d\n", readnum);
+  fprintf(stderr, "len(buf): %d\n", strlen(buf));
+  fprintf(stderr, "buf: [%s]\n", buf);
+  return 0;
+}
 
 int main(int ac, char **av)
 {
@@ -19,5 +38,6 @@ int main(int ac, char **av)
     << sizeof(size_t) << endl;
   for(i = 0; i < ac; ++i)
     cout << i << " " << av[i] << endl;
+  test_sscanf();
   return 0;
 }
